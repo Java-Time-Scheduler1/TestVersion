38422e416229524993c769274be3ced4a88f4f7b
MJ-Esm <75901778+MJ-Esm@users.noreply.github.com>
2020-12-12 18:49:10 +0100

    alles hat soweit super geklappt

--- src/TestVersion.java
+++ src/TestVersion.java
@@ -7,6 +7,8 @@ public class TestVersion {
     public static void main(String[] args) {
         System.out.println("TestVersion  IGNORIEREN");
         System.out.println("Kannst du das sehen Mohammadjawad?");
+        System.out.println("JA ich kann, alles Perfekt");
+
     }
     
 }
