601c2584d121cac0eaa0d2a39a5a9a58dbf6fe70
Weaam19 <53410080+Weaam19@users.noreply.github.com>
2020-12-18 15:47:12 +0100

    Update Main.java

--- TimeScheduler/src/main/java/com/mycompany/timescheduler/Main.java
+++ TimeScheduler/src/main/java/com/mycompany/timescheduler/Main.java
@@ -2,6 +2,8 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ 
+Test from Weiam 
  */
 package com.mycompany.timescheduler;
 import java.util.Scanner;
